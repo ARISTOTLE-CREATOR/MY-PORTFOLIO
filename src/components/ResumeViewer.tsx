7e97d4ab3a8a7a40d9d73c338f9b1de2ab7ca8b8
R_SAI NITHIN <sainithin172005@gmail.com>
2026-08-10 08:25:34 +0530

    Add ResumeViewer component to display resume PDF

--- src/components/ResumeViewer.tsx
+++ src/components/ResumeViewer.tsx
@@ -1,1 +1,14 @@
 
+const ResumeViewer = () => {
+  return (
+    <div className="min-h-screen w-full bg-white">
+      <iframe
+        src="/resume.pdf"
+        title="Resume"
+        className="w-full h-screen border-0"
+      />
+    </div>
+  );
+};
+
+export default ResumeViewer;
